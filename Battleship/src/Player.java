2bde6ee8bbcfab59a3a4248b0de587301e621254
nicholas.dohr <nicholas.dohr@MU-C1-09.mullen.pvt.k12.co.us>
2022-02-16 10:15:06 -0700

    updated Code

--- Battleship/src/Player.java
+++ Battleship/src/Player.java
@@ -1,5 +1,148 @@
 
 public class Player
 	{
-
+		 String [][] checkBoard = new String [10][10];
+		 String [][] showBoard = new String [10][10];
+		 int numOfHits = 0;
+		 int numOfMisses = 0;
+		 int numOfShipsSunk = 0;
+		 int numOfShips;
+		String name;
+		
+		public String[][] getCheckBoard()
+			{
+				return checkBoard;
+			}
+		public void setCheckBoard(String[][] checkBoard)
+			{
+				this.checkBoard = checkBoard;
+			}
+		public String[][] getShowBoard()
+			{
+				return showBoard;
+			}
+		public void setShowBoard(String[][] showBoard)
+			{
+				this.showBoard = showBoard;
+			}
+		public int getNumOfHits()
+			{
+				return numOfHits;
+			}
+		public void setNumOfHits(int numOfHits)
+			{
+				this.numOfHits = numOfHits;
+			}
+		public int getNumOfMisses()
+			{
+				return numOfMisses;
+			}
+		public void setNumOfMisses(int numOfMisses)
+			{
+				this.numOfMisses = numOfMisses;
+			}
+		public int getNumOfShipsSunk()
+			{
+				return numOfShipsSunk;
+			}
+		public void setNumOfShipsSunk(int numOfShipsSunk)
+			{
+				this.numOfShipsSunk = numOfShipsSunk;
+			}
+		public int getNumOfShips()
+			{
+				return numOfShips;
+			}
+		public void setNumOfShips(int numOfShips)
+			{
+				this.numOfShips = numOfShips;
+			}
+		public String getName()
+			{
+				return name;
+			}
+		public void setName(String name)
+			{
+				this.name = name;
+			}
+		public static void fillShowBoard(Player p)
+			{
+				for (int row = 0; row < p.showBoard.length; row++)
+					{
+						   for (int col = 0; col < p.showBoard[0].length; col++)
+							   {
+								   p.showBoard[row][col] = " ";
+							   }
+					}
+				
+				
+			}
+		public static void displayShowBoard(Player p)
+			{
+				System.out.println("     1     2     3     4     5     6     7     8     9     10");
+				System.out.println("  -------------------------------------------------------------");
+				System.out.print("A |  " + p.showBoard[0][0] + "  |  " + p.showBoard [0][1] + "  |  "  + p.showBoard [0][2] + "  |  " + p.showBoard [0][3] + "  |  " + p.showBoard [0][4] + "  |  " + p.showBoard [0][5] + "  |  ");
+				System.out.println( p.showBoard[0][6] + "  |  " + p.showBoard [0][7] + "  |  "  + p.showBoard [0][8] + "  |  " + p.showBoard [0][9] + "  |  ");
+				System.out.println("  -------------------------------------------------------------");
+				System.out.print("B |  " + p.showBoard[1][0] + "  |  " + p.showBoard [1][1] + "  |  "  + p.showBoard [1][2] + "  |  " + p.showBoard [1][3] + "  |  " + p.showBoard [1][4] + "  |  " + p.showBoard [1][5] + "  |  ");
+				System.out.println( p.showBoard[1][6] + "  |  " + p.showBoard [1][7] + "  |  "  + p.showBoard [1][8] + "  |  " + p.showBoard [1][9] + "  |  ");
+				System.out.println("  -------------------------------------------------------------");
+				System.out.print("C |  " + p.showBoard[2][0] + "  |  " + p.showBoard [2][1] + "  |  "  + p.showBoard [2][2] + "  |  " + p.showBoard [2][3] + "  |  " + p.showBoard [2][4] + "  |  " + p.showBoard [2][5] + "  |  ");
+				System.out.println( p.showBoard[2][6] + "  |  " + p.showBoard [2][7] + "  |  "  + p.showBoard [2][8] + "  |  " + p.showBoard [2][9] + "  |  ");
+				System.out.println("  -------------------------------------------------------------");
+				System.out.print("D |  " + p.showBoard[3][0] + "  |  " + p.showBoard [3][1] + "  |  "  + p.showBoard [3][2] + "  |  " + p.showBoard [3][3] + "  |  " + p.showBoard [3][4] + "  |  " + p.showBoard [3][5] + "  |  ");
+				System.out.println( p.showBoard[3][6] + "  |  " + p.showBoard [3][7] + "  |  "  + p.showBoard [3][8] + "  |  " + p.showBoard [3][9] + "  |  ");
+				System.out.println("  -------------------------------------------------------------");
+				System.out.print("E |  " + p.showBoard[4][0] + "  |  " + p.showBoard [4][1] + "  |  "  + p.showBoard [4][2] + "  |  " + p.showBoard [4][3] + "  |  " + p.showBoard [4][4] + "  |  " + p.showBoard [4][5] + "  |  ");
+				System.out.println( p.showBoard[4][6] + "  |  " + p.showBoard [4][7] + "  |  "  + p.showBoard [4][8] + "  |  " + p.showBoard [4][9] + "  |  ");
+				System.out.println("  -------------------------------------------------------------");
+				System.out.print("F |  " + p.showBoard[5][0] + "  |  " + p.showBoard [5][1] + "  |  "  + p.showBoard [5][2] + "  |  " + p.showBoard [5][3] + "  |  " + p.showBoard [5][4] + "  |  " + p.showBoard [5][5] + "  |  ");
+				System.out.println( p.showBoard[5][6] + "  |  " + p.showBoard [5][7] + "  |  "  + p.showBoard [5][8] + "  |  " + p.showBoard [5][9] + "  |  ");
+				System.out.println("  -------------------------------------------------------------");
+				System.out.print("G |  " + p.showBoard[6][0] + "  |  " + p.showBoard [6][1] + "  |  "  + p.showBoard [6][2] + "  |  " + p.showBoard [6][3] + "  |  " + p.showBoard [6][4] + "  |  " + p.showBoard [6][5] + "  |  ");
+				System.out.println( p.showBoard[6][6] + "  |  " + p.showBoard [6][7] + "  |  "  + p.showBoard [6][8] + "  |  " + p.showBoard [6][9] + "  |  ");
+				System.out.println("  -------------------------------------------------------------");
+				System.out.print("H |  " + p.showBoard[7][0] + "  |  " + p.showBoard [7][1] + "  |  "  + p.showBoard [7][2] + "  |  " + p.showBoard [7][3] + "  |  " + p.showBoard [7][4] + "  |  " + p.showBoard [7][5] + "  |  ");
+				System.out.println( p.showBoard[7][6] + "  |  " + p.showBoard [7][7] + "  |  "  + p.showBoard [7][8] + "  |  " + p.showBoard [7][9] + "  |  ");
+				System.out.println("  -------------------------------------------------------------");
+				System.out.print("I |  " + p.showBoard[8][0] + "  |  " + p.showBoard [8][1] + "  |  "  + p.showBoard [8][2] + "  |  " + p.showBoard [8][3] + "  |  " + p.showBoard [8][4] + "  |  " + p.showBoard [8][5] + "  |  ");
+				System.out.println( p.showBoard[8][6] + "  |  " + p.showBoard [8][7] + "  |  "  + p.showBoard [8][8] + "  |  " + p.showBoard [8][9] + "  |  ");
+				System.out.println("  -------------------------------------------------------------");
+				System.out.print("J |  " + p.showBoard[9][0] + "  |  " + p.showBoard [9][1] + "  |  "  + p.showBoard [9][2] + "  |  " + p.showBoard [9][3] + "  |  " + p.showBoard [9][4] + "  |  " + p.showBoard [9][5] + "  |  ");
+				System.out.println( p.showBoard[9][6] + "  |  " + p.showBoard [9][7] + "  |  "  + p.showBoard [9][8] + "  |  " + p.showBoard [9][9] + "  |  ");
+				System.out.println("  -------------------------------------------------------------");
+				
+				
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+						System.out.println();
+					
+			}
+		
 	}
--- Battleship/src/ShipPlacements.java
+++ Battleship/src/ShipPlacements.java
@@ -1,171 +1,255 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ShipPlacements
 	{
-		public static void shipDecisions()
+		public static void shipDecisions(Player p)
 		{
+			ArrayList<String> options = new ArrayList<String>();
+			options.add("Option 1");
+			options.add("Option 2");
+			options.add("Option 3");
+			options.add("Option 4");
+			options.add("Option 5");
 			Scanner userIntInput = new Scanner(System.in);
 			Scanner userStringInput = new Scanner(System.in);
-			System.out.println("Player 1, you have 4 board choices");
-			System.out.println("Which option do you want to display");
-			System.out.println("Option 1");
-			System.out.println("Option 2");
-			System.out.println("Option 3");
-			System.out.println("Option 4");
-			System.out.println("Option 5");
-			System.out.println("Which one do you want to choose?");
-			int choice = userIntInput.nextInt();
+			System.out.println(p.getName() + ", your board is being randomly generated now");
+			
+			
+			int choice = (int) (Math.random() * 5) + 1;
 			if(choice == 1)
 				{
-			System.out.println("Here is option 1");
-			Runner.checkBoard[0][1] = "X";
-			Runner.checkBoard[0][2] = "X";
-			Runner.checkBoard[0][3] = "X";
-			Runner.checkBoard[0][4] = "X";
-			Runner.checkBoard[0][5] = "X";
+				System.out.println("Here is option 1");
+				p.checkBoard[0][1] = "X";
+				p.checkBoard[0][2] = "X";
+				p.checkBoard[0][3] = "X";
+				p.checkBoard[0][4] = "X";
 			
-			Runner.checkBoard[6][0] = "X";
-			Runner.checkBoard[7][0] = "X";
-			Runner.checkBoard[8][0] = "X";
-			Runner.checkBoard[9][0] = "X";
 			
-			Runner.checkBoard[2][9] = "X";
-			Runner.checkBoard[3][9] = "X";
-			Runner.checkBoard[4][9] = "X";
-			Runner.checkBoard[5][9] = "X";
+				p.checkBoard[6][0] = "X";
+				p.checkBoard[7][0] = "X";
+				p.checkBoard[8][0] = "X";
+				p.checkBoard[9][0] = "X";
 			
-			Runner.checkBoard[2][3] = "X";
-			Runner.checkBoard[2][4] = "X";
-			Runner.checkBoard[2][5] = "X";
+				p.checkBoard[2][9] = "X";
+				p.checkBoard[3][9] = "X";
+				p.checkBoard[4][9] = "X";
+				p.checkBoard[5][9] = "X";
 			
-			Runner.checkBoard[6][5] = "X";
-			Runner.checkBoard[6][6] = "X";
-			Runner.checkBoard[6][7] = "X";
+				p.checkBoard[2][3] = "X";
+				p.checkBoard[2][4] = "X";
+				p.checkBoard[2][5] = "X";
+				p.checkBoard[2][6] = "X";
 			
-			Runner.checkBoard[8][5] = "X";
-			Runner.checkBoard[8][6] = "X";
-			Runner.displayBoard();
+				p.checkBoard[6][4] = "X";
+				p.checkBoard[6][5] = "X";
+				p.checkBoard[6][6] = "X";
+				p.checkBoard[6][7] = "X";
+			
+			
+				p.checkBoard[8][5] = "X";
+				p.checkBoard[8][6] = "X";
+				p.checkBoard[8][7] = "X";
+				p.checkBoard[8][8] = "X";
+				options.remove(0);
+				Runner.displayBoard(p);
 				}
 			else if(choice == 2)
 				{
-					Runner.checkBoard[0][0] = "X";
-					Runner.checkBoard[1][0] = "X";
-					Runner.checkBoard[2][0] = "X";
-					Runner.checkBoard[3][0] = "X";
-					Runner.checkBoard[4][0] = "X";
+					p.checkBoard[0][0] = "X";
+					p.checkBoard[1][0] = "X";
+					p.checkBoard[2][0] = "X";
+					p.checkBoard[3][0] = "X";
 					
-					Runner.checkBoard[6][2] = "X";
-					Runner.checkBoard[7][2] = "X";
-					Runner.checkBoard[8][2] = "X";
-					Runner.checkBoard[9][2] = "X";
 					
-					Runner.checkBoard[0][9] = "X";
-					Runner.checkBoard[1][9] = "X";
-					Runner.checkBoard[2][9] = "X";
-					Runner.checkBoard[3][9] = "X";
+					p.checkBoard[6][2] = "X";
+					p.checkBoard[7][2] = "X";
+					p.checkBoard[8][2] = "X";
+					p.checkBoard[9][2] = "X";
 					
-					Runner.checkBoard[1][3] = "X";
-					Runner.checkBoard[1][4] = "X";
-					Runner.checkBoard[1][5] = "X";
+					p.checkBoard[0][9] = "X";
+					p.checkBoard[1][9] = "X";
+					p.checkBoard[2][9] = "X";
+					p.checkBoard[3][9] = "X";
 					
-					Runner.checkBoard[9][5] = "X";
-					Runner.checkBoard[9][6] = "X";
-					Runner.checkBoard[9][7] = "X";
+					p.checkBoard[1][3] = "X";
+					p.checkBoard[1][4] = "X";
+					p.checkBoard[1][5] = "X";
+					p.checkBoard[1][6] = "X";
 					
-					Runner.checkBoard[6][5] = "X";
-					Runner.checkBoard[6][6] = "X";
-					Runner.displayBoard();
+					p.checkBoard[9][5] = "X";
+					p.checkBoard[9][6] = "X";
+					p.checkBoard[9][7] = "X";
+					p.checkBoard[9][8] = "X";
+					
+					p.checkBoard[6][5] = "X";
+					p.checkBoard[6][6] = "X";
+					p.checkBoard[6][7] = "X";
+					p.checkBoard[6][8] = "X";
+					options.remove(1);
+					Runner.displayBoard(p);
 				}
 			else if(choice == 3)
 				{
-					Runner.checkBoard[0][1] = "X";
-					Runner.checkBoard[0][2] = "X";
-					Runner.checkBoard[0][3] = "X";
-					Runner.checkBoard[0][4] = "X";
-					Runner.checkBoard[0][5] = "X";
+					p.checkBoard[0][1] = "X";
+					p.checkBoard[0][2] = "X";
+					p.checkBoard[0][3] = "X";
+					p.checkBoard[0][4] = "X";
 					
-					Runner.checkBoard[2][5] = "X";
-					Runner.checkBoard[2][6] = "X";
-					Runner.checkBoard[2][7] = "X";
-					Runner.checkBoard[2][8] = "X";
 					
-					Runner.checkBoard[4][6] = "X";
-					Runner.checkBoard[4][7] = "X";
-					Runner.checkBoard[4][8] = "X";
-					Runner.checkBoard[4][9] = "X";
+					p.checkBoard[2][5] = "X";
+					p.checkBoard[2][6] = "X";
+					p.checkBoard[2][7] = "X";
+					p.checkBoard[2][8] = "X";
 					
-					Runner.checkBoard[4][1] = "X";
-					Runner.checkBoard[4][2] = "X";
-					Runner.checkBoard[4][1] = "X";
+					p.checkBoard[4][6] = "X";
+					p.checkBoard[4][7] = "X";
+					p.checkBoard[4][8] = "X";
+					p.checkBoard[4][9] = "X";
 					
-					Runner.checkBoard[7][5] = "X";
-					Runner.checkBoard[7][6] = "X";
-					Runner.checkBoard[7][7] = "X";
+					p.checkBoard[4][1] = "X";
+					p.checkBoard[4][2] = "X";
+					p.checkBoard[4][3] = "X";
+					p.checkBoard[4][4] = "X";
 					
-					Runner.checkBoard[9][5] = "X";
-					Runner.checkBoard[9][6] = "X";
-					Runner.displayBoard();
+					p.checkBoard[7][4] = "X";
+					p.checkBoard[7][5] = "X";
+					p.checkBoard[7][6] = "X";
+					p.checkBoard[7][7] = "X";
+					
+					p.checkBoard[9][5] = "X";
+					p.checkBoard[9][6] = "X";
+					p.checkBoard[9][7] = "X";
+					p.checkBoard[9][8] = "X";
+					options.remove(2);
+					Runner.displayBoard(p);
 				}
 			else if(choice == 4)
 				{
-					Runner.checkBoard[5][1] = "X";
-					Runner.checkBoard[6][1] = "X";
-					Runner.checkBoard[7][1] = "X";
-					Runner.checkBoard[8][1] = "X";
-					Runner.checkBoard[9][1] = "X";
 					
-					Runner.checkBoard[1][3] = "X";
-					Runner.checkBoard[2][3] = "X";
-					Runner.checkBoard[3][3] = "X";
-					Runner.checkBoard[4][3] = "X";
+					p.checkBoard[6][1] = "X";
+					p.checkBoard[7][1] = "X";
+					p.checkBoard[8][1] = "X";
+					p.checkBoard[9][1] = "X";
 					
-					Runner.checkBoard[5][5] = "X";
-					Runner.checkBoard[6][5] = "X";
-					Runner.checkBoard[7][5] = "X";
-					Runner.checkBoard[8][5] = "X";
+					p.checkBoard[1][3] = "X";
+					p.checkBoard[2][3] = "X";
+					p.checkBoard[3][3] = "X";
+					p.checkBoard[4][3] = "X";
 					
-					Runner.checkBoard[1][7] = "X";
-					Runner.checkBoard[2][7] = "X";
-					Runner.checkBoard[3][7] = "X";
+					p.checkBoard[5][5] = "X";
+					p.checkBoard[6][5] = "X";
+					p.checkBoard[7][5] = "X";
+					p.checkBoard[8][5] = "X";
 					
-					Runner.checkBoard[3][9] = "X";
-					Runner.checkBoard[4][9] = "X";
-					Runner.checkBoard[5][9] = "X";
+					p.checkBoard[1][7] = "X";
+					p.checkBoard[2][7] = "X";
+					p.checkBoard[3][7] = "X";
+					p.checkBoard[4][7] = "X";
 					
-					Runner.checkBoard[1][1] = "X";
-					Runner.checkBoard[2][1] = "X";
-					Runner.displayBoard();
+					p.checkBoard[3][9] = "X";
+					p.checkBoard[4][9] = "X";
+					p.checkBoard[5][9] = "X";
+					p.checkBoard[6][9] = "X";
+					
+					p.checkBoard[1][1] = "X";
+					p.checkBoard[2][1] = "X";
+					p.checkBoard[3][1] = "X";
+					p.checkBoard[4][1] = "X";
+					options.remove(3);
+					Runner.displayBoard(p);
 				}
 			else if(choice == 5)
 				{
-					Runner.checkBoard[5][4] = "X";
-					Runner.checkBoard[6][4] = "X";
-					Runner.checkBoard[7][4] = "X";
-					Runner.checkBoard[8][4] = "X";
-					Runner.checkBoard[9][4] = "X";
+					p.checkBoard[5][4] = "X";
+					p.checkBoard[6][4] = "X";
+					p.checkBoard[7][4] = "X";
+					p.checkBoard[8][4] = "X";
 					
-					Runner.checkBoard[1][2] = "X";
-					Runner.checkBoard[2][2] = "X";
-					Runner.checkBoard[3][2] = "X";
-					Runner.checkBoard[4][2] = "X";
 					
-					Runner.checkBoard[7][6] = "X";
-					Runner.checkBoard[7][7] = "X";
-					Runner.checkBoard[7][8] = "X";
-					Runner.checkBoard[7][9] = "X";
+					p.checkBoard[1][2] = "X";
+					p.checkBoard[2][2] = "X";
+					p.checkBoard[3][2] = "X";
+					p.checkBoard[4][2] = "X";
 					
-					Runner.checkBoard[1][7] = "X";
-					Runner.checkBoard[2][7] = "X";
-					Runner.checkBoard[3][7] = "X";
+					p.checkBoard[7][6] = "X";
+					p.checkBoard[7][7] = "X";
+					p.checkBoard[7][8] = "X";
+					p.checkBoard[7][9] = "X";
 					
-					Runner.checkBoard[3][9] = "X";
-					Runner.checkBoard[4][9] = "X";
-					Runner.checkBoard[5][9] = "X";
+					p.checkBoard[1][7] = "X";
+					p.checkBoard[2][7] = "X";
+					p.checkBoard[3][7] = "X";
+					p.checkBoard[4][7] = "X";
 					
-					Runner.checkBoard[1][0] = "X";
-					Runner.checkBoard[2][0] = "X";
-					Runner.displayBoard();
+					p.checkBoard[3][9] = "X";
+					p.checkBoard[4][9] = "X";
+					p.checkBoard[5][9] = "X";
+					p.checkBoard[6][9] = "X";
+					
+					p.checkBoard[1][0] = "X";
+					p.checkBoard[2][0] = "X";
+					p.checkBoard[3][0] = "X";
+					p.checkBoard[4][0] = "X";
+					options.remove(4);
+					Runner.displayBoard(p);
 				}
+			Runner.pause();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			
 		}
 		}
 	
